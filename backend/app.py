7117c22529201d2874893b40bf3ef1d9bf0f9270
gprok <gprok60@gmail.com>
2025-10-22 23:45:02 +0300

    flask dynamic pages

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -15,7 +16,19 @@ def services():
 def products():
     return render_template('products.html')
 
+@app.route('/products/2')
+def products2():
+    data = open('static/products.txt', 'r').readlines()
+    
+    products_list = []
+    id = 1;
+    for line in data:
+        products_list.append({'name': line.strip(), 'id': id})
+        id = id + 1
+    print(products_list)
+    return render_template('products2.html', products=products_list)
+
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
